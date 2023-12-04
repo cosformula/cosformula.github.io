20bcdecc0707a621f526f1d230cdc690592fd88c
cosformula <cosformula@gmail.com>
2023-12-04 13:45:59 +0800

    update

--- components/BlogNotionRenderer.js
+++ components/BlogNotionRenderer.js
@@ -1,12 +1,12 @@
 import { Collection, CollectionRow, NotionRenderer, Code, useNotionContext } from 'react-notion-x'
-const CDN_HOST = 'https://image-store-1251724012.file.myqcloud.com/release/'
+// const CDN_HOST = 'https://image-store-1251724012.file.myqcloud.com/release/'
 
-const encodeBase64Url = (str) => {
-  return Buffer.from(str).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
-}
+// const encodeBase64Url = (str) => {
+//   return Buffer.from(str).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+// }
 
 export const BlogNotionRenderer = (props) => {
-  const { mapImageUrl } = useNotionContext()
+  // const { mapImageUrl } = useNotionContext()
   const { components, ...rest } = props
   return (
     <NotionRenderer
