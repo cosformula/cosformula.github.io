34be3340d5508e4b4be331eacc5548fa7013d97e
cosformula <cosformula@gmail.com>
2023-12-04 13:43:54 +0800

    update

--- components/BlogNotionRenderer.js
+++ components/BlogNotionRenderer.js
@@ -23,17 +23,18 @@ export const BlogNotionRenderer = (props) => {
       }}
       // customImages
       // rootPageId={process.env.NOTION_ROOT_PAGE_ID}
-      mapImageUrl={(url, block) => {
-        const signedUrl = new URL(mapImageUrl(url, block))
-        if (process.env.VERCEL_ENV) {
-          // console.log('vercel env')
-          return signedUrl
-        }
-        const urlParts = signedUrl.pathname.split('/')
-        const realUrl = `${decodeURIComponent(urlParts[2])}`
+      // mapImageUrl={(url, block) => {
+      //   const signedUrl = new URL(mapImageUrl(url, block))
 
-        return `${CDN_HOST}${encodeURIComponent(encodeBase64Url(realUrl))}${signedUrl.search}`
-      }}
+      //   if (process.env.VERCEL_ENV) {
+      //     // console.log('vercel env')
+      //     return signedUrl
+      //   }
+      //   const urlParts = signedUrl.pathname.split('/')
+      //   const realUrl = `${decodeURIComponent(urlParts[2])}`
+
+      //   return `${CDN_HOST}${encodeURIComponent(encodeBase64Url(realUrl))}${signedUrl.search}`
+      // }}
       {...rest}
     />
   )
