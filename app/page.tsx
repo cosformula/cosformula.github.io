bcfce9e5c6b6d1445cdba6f3f5cd9a6bf9d57ce2
cosformula <cosformula@gmail.com>
2025-03-14 21:42:37 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        {/* My Portfolio */}
+        cosformula
       </h1>
       <p className="mb-4">
-        {`A developer`}
+        {`产品工程师，游戏开发者。`}
       </p>
       <div className="my-8">
         <BlogPosts />
