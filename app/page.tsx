c7d18a5a573cf9b4a54522a7505f7dc6a5bd2fdf
cosformula <cosformula@gmail.com>
2025-04-02 10:12:22 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ export default function Page() {
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         cosformula
-
       </h1>
-      <p className="mb-4">
+      {/* <p className="mb-4">
       <a href="https://wakatime.com/@41fdecfe-e524-4e64-a686-e00e43a57b8c"><img src="https://wakatime.com/badge/user/41fdecfe-e524-4e64-a686-e00e43a57b8c.svg" alt="Total time coded since Aug 1 2019" /></a>
-      </p>
-
+      </p> */}
       <p className="mb-4">
         {`产品工程师，游戏开发者。`}
+      </p>
+      <p className="mb-4">
+        {`Product Engineer, Game Developer.`}
       </p>
       <div className="my-8">
         <BlogPosts />
