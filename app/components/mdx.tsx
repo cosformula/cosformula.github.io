1bcef06aeb741a88039ef9203bd539aa9a9c194f
cosformula <cosformula@gmail.com>
2025-04-05 06:11:43 +0800

    update

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -62,14 +62,16 @@ function Code({ children, ...props }) {
 }
 
 function slugify(str) {
-  return str
+  let slug = str
     .toString()
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
     .replace(/\s+/g, '-') // Replace spaces with -
     .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    // .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
+  console.log(slug)
+  return slug
 }
 
 function createHeading(level) {
@@ -130,7 +132,7 @@ export function CustomMDX(props) {
         mdxOptions: {
           remarkPlugins: [
             remarkMath,
-            // remarkToc,
+            remarkToc,
             // remarkGfm,
             remarkCallout,
             // remarkObsidian
