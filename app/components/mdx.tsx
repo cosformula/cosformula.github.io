d34503294f65e94e8b87280c6bb68c614ad4b8df
cosformula <cosformula@gmail.com>
2025-04-07 10:20:57 +0800

    update

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -70,7 +70,7 @@ function slugify(str) {
     .replace(/&/g, '-and-') // Replace & with 'and'
     // .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
-  console.log(slug)
+  // console.log(slug)
   return slug
 }
 
